6ca934954fc5539c34d31f065438fd3af7fd0275
Om Patel <ompatel34003@gmail.com>
2025-07-29 11:55:00 +0530

    new

--- server/controllers/admin/products-controller.js
+++ server/controllers/admin/products-controller.js
@@ -164,84 +164,153 @@
 
 
 
+import { imageUploadUtil } from "../../helpers/cloudinary.js";
+import Product from "../../models/Product.js";
 
-import Order from "../../models/Order.js";
-
-export const getAllOrdersOfAllUsers = async (req, res) => {
+export const handleImageUpload = async (req, res) => {
     try {
-        const orders = await Order.find({});
+        const b64 = Buffer.from(req.file.buffer).toString("base64");
+        const url = "data:" + req.file.mimetype + ";base64," + b64;
+        const result = await imageUploadUtil(url);
 
-        if (!orders.length) {
-            return res.status(404).json({
-                success: false,
-                message: "No orders found!",
-            });
-        }
-
-        res.status(200).json({
+        res.json({
             success: true,
-            data: orders,
+            result,
         });
-    } catch (e) {
-        console.log(e);
-        res.status(500).json({
+    } catch (error) {
+        console.log(error);
+        res.json({
             success: false,
-            message: "Some error occurred!",
+            message: "Error occured",
         });
     }
 };
 
-export const getOrderDetailsForAdmin = async (req, res) => {
+export const addProduct = async (req, res) => {
     try {
-        const { id } = req.params;
+        const {
+            image,
+            title,
+            description,
+            category,
+            brand,
+            price,
+            salePrice,
+            totalStock,
+            averageReview,
+        } = req.body;
 
-        const order = await Order.findById(id);
+        console.log(averageReview, "averageReview");
 
-        if (!order) {
-            return res.status(404).json({
-                success: false,
-                message: "Order not found!",
-            });
-        }
+        const newlyCreatedProduct = new Product({
+            image,
+            title,
+            description,
+            category,
+            brand,
+            price,
+            salePrice,
+            totalStock,
+            averageReview,
+        });
 
-        res.status(200).json({
+        await newlyCreatedProduct.save();
+        res.status(201).json({
             success: true,
-            data: order,
+            data: newlyCreatedProduct,
         });
     } catch (e) {
         console.log(e);
         res.status(500).json({
             success: false,
-            message: "Some error occurred!",
+            message: "Error occured",
         });
     }
 };
 
-export const updateOrderStatus = async (req, res) => {
+export const fetchAllProducts = async (req, res) => {
     try {
-        const { id } = req.params;
-        const { orderStatus } = req.body;
-
-        const order = await Order.findById(id);
-
-        if (!order) {
-            return res.status(404).json({
-                success: false,
-                message: "Order not found!",
-            });
-        }
-
-        await Order.findByIdAndUpdate(id, { orderStatus });
-
+        const listOfProducts = await Product.find({});
         res.status(200).json({
             success: true,
-            message: "Order status is updated successfully!",
+            data: listOfProducts,
         });
     } catch (e) {
         console.log(e);
         res.status(500).json({
             success: false,
-            message: "Some error occurred!",
+            message: "Error occured",
+        });
+    }
+};
+
+export const editProduct = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const {
+            image,
+            title,
+            description,
+            category,
+            brand,
+            price,
+            salePrice,
+            totalStock,
+            averageReview,
+        } = req.body;
+
+        let findProduct = await Product.findById(id);
+        if (!findProduct)
+            return res.status(404).json({
+                success: false,
+                message: "Product not found",
+            });
+
+        findProduct.title = title || findProduct.title;
+        findProduct.description = description || findProduct.description;
+        findProduct.category = category || findProduct.category;
+        findProduct.brand = brand || findProduct.brand;
+        findProduct.price = price === "" ? 0 : price || findProduct.price;
+        findProduct.salePrice =
+            salePrice === "" ? 0 : salePrice || findProduct.salePrice;
+        findProduct.totalStock = totalStock || findProduct.totalStock;
+        findProduct.image = image || findProduct.image;
+        findProduct.averageReview = averageReview || findProduct.averageReview;
+
+        await findProduct.save();
+        res.status(200).json({
+            success: true,
+            data: findProduct,
+        });
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({
+            success: false,
+            message: "Error occured",
+        });
+    }
+};
+
+export const deleteProduct = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const product = await Product.findByIdAndDelete(id);
+
+        if (!product)
+            return res.status(404).json({
+                success: false,
+                message: "Product not found",
+            });
+
+        res.status(200).json({
+            success: true,
+            message: "Product deleted successfully",
+        });
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({
+            success: false,
+            message: "Error occured",
         });
     }
 };
