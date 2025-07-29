6f0a1199e5d2213911c88dac4cb1528c7a5b68d6
Om Patel <ompatel34003@gmail.com>
2025-07-29 10:42:54 +0530

    Trying to deploy on render

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,6 +6,7 @@ import tailwindcss from "@tailwindcss/vite"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "./", // this is important for correct asset paths on Render
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
