ddede97082450d78ea040aea4f19c7df42776bd6
Om Patel <ompatel34003@gmail.com>
2025-07-29 12:16:13 +0530

    new

--- server/server.js
+++ server/server.js
@@ -103,12 +103,12 @@ mongoose.connect(process.env.MONGO_URI)
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-if (process.env.NODE_ENV !== "production") {
+// if (process.env.NODE_ENV !== "production") {
     app.use(cors({
-        origin: process.env.CLIENT_ORIGIN || "http://localhost:5173",
+        origin: "https://e-commerce-plateform.onrender.com" || process.env.CLIENT_ORIGIN || "http://localhost:5173",
         credentials: true,
     }));
-}
+// }
 
 app.use(cookieParser());
 app.use(express.json());
